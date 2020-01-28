c3b5bb5357cac6d83e12a7a1968c8fc96a3f1d3c
shane <305301+whardier@users.noreply.github.com>
2020-01-28 13:09:51 -0900

    Added icon and a bit of test code

--- stagehand/stagehand.py
+++ stagehand/stagehand.py
@@ -1,1 +1,26 @@
 
+#!/usr/bin/env python
+import os
+import wx
+
+app = wx.App(False)
+
+
+menuBar = wx.MenuBar()
+file_menu = wx.Menu()
+app.SetMacExitMenuItemId(wx.ID_EXIT)
+exit_item = file_menu.Append(wx.ID_EXIT, "E&xit\tCtrl-Q", "Exit NodeMCU PyFlasher")
+menuBar.Append(file_menu, "&File")
+
+scale = dir(app.GetTopWindow())
+
+frame = wx.Frame(None, wx.ID_ANY, "stagehand")
+
+frame.SetIcon(wx.Icon("../resource/icon-app.ico"))
+frame.Show(True)
+
+frame.SetMenuBar(menuBar)
+
+print('hi')
+
+app.MainLoop()
